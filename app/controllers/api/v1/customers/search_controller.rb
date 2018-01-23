a4cae7e8e3a7976495633e1554d5f85526b92500
maxscores <maxstackhouse@gmail.com>
2018-01-23 15:05:45 -0700

    Hacker

---
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::Customers::SearchController < ApplicationController
+
+
+  def show
+    render json: Customer.where(customer_params).first
+  end
+
+  def index
+    render json: Customer.where(customer_params)
+  end
+
+  private
+
+  def customer_params
+    params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
+  end
+end
