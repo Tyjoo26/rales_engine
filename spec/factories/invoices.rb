99650fda2f1cff817fc41e79f41c51749dbef388
Max Stackhouse <maxstackhouse@gmail.com>
2018-01-23 12:24:18 -0700

    Revert "Invoice record endpoint"

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
   factory :invoice do
-    customer
-    merchant
-    status 0
-    created_at "2012-03-06T16:54:31.000Z"
-    updated_at "2012-03-06T16:54:31.000Z"
+    customer nil
+    merchant nil
+    status 1
   end
 end
--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :merchant do
     name "Mile High Dispensary"
-    created_at "2012-03-06T16:54:31.000Z"
-    updated_at "2012-03-06T16:54:31.000Z"
   end
 end
