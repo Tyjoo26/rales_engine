bfa742d88a3c3fdef01fe3e8aca93559e221480b
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-22 18:53:05 -0700

    name space route for search functionality for customer

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,18 @@ Rails.application.routes.draw do
 
 
 
+
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
       resources :transactions, only: [:index, :show]
+      namespace :customers do
+        get '/find', to: "search#show"
+        get '/find_all', to: "search#index"
+      end
       resources :customers, only: [:index, :show ]
     end
   end
+
+
 end
