c226f72be2573905e79ac3e7c202711a8664ac81
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-23 13:48:38 -0700

    new name space route for transactions

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,21 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: "search#show"
+        get '/find_all', to: "search#index"
+      end
       resources :merchants, only: [:index, :show]
-      resources :transactions, only: [:index, :show]
       namespace :customers do
         get '/find', to: "search#show"
         get '/find_all', to: "search#index"
       end
       resources :customers, only: [:index, :show ]
+      namespace :transactions do
+        get '/find', to: "search#show"
+        get '/find_all', to: "search#index"
+      end
+      resources :transactions, only: [:index, :show]
     end
   end
 
