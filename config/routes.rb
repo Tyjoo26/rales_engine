2eb0fe18997a2cb02b105cde30150f1b6e1804ad
maxscores <maxstackhouse@gmail.com>
2018-01-22 19:17:11 -0700

    Add find route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
+      namespace :invoices do
+        get '/find', to: 'search#show'
+      end
+      resources :invoices, only: [:index, :show]
     end
   end
 end
