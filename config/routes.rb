9bd41b8b7260c2cc09381e0ea27123a07ae8e999
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-23 14:30:00 -0700

    Modify routes file by adding random custom route to each name space transactions customers merchants

--- config/routes.rb
+++ config/routes.rb
@@ -4,27 +4,29 @@ Rails.application.routes.draw do
 
 
 
+  get 'random/show'
+
   namespace :api do
     namespace :v1 do
       namespace :merchants do
         get '', to: "merchants#index"
         get '/find', to: "search#show"
         get '/find_all', to: "search#index"
-        get '/random' to: "random#show"
+        get '/random', to: "random#show"
         get '/:id', to: "merchants#show"
       end
       namespace :customers do
         get '', to: "customers#index"
         get '/find', to: "search#show"
         get '/find_all', to: "search#index"
-        get '/random' to: "random#show"
+        get '/random', to: "random#show"
         get '/:id', to: "customers#show"
       end
       namespace :transactions do
         get '', to: "transactions#index"
         get '/find', to: "search#show"
         get '/find_all', to: "search#index"
-        get '/random' to: "random#show"
+        get '/random', to: "random#show"
         get '/:id', to: "transactions#show"
       end
     end
