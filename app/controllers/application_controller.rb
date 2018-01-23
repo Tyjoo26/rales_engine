d8e01b7c30ed0226747edfb94e1827b1742dea85
maxscores <maxstackhouse@gmail.com>
2018-01-23 12:54:01 -0700

    remove redundant to_i

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def price_to_integer(price_as_string)
-    (JSON.parse(price_as_string).to_f * 100.00).round(0).to_i
+    (JSON.parse(price_as_string).to_f * 100.00).round(0)
   end
 end
