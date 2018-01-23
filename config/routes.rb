162438d1eab485a25a05ea505b3d51f84440fac7
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-23 14:25:33 -0700

    Add custom route for random to merchatns transactions and customers

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,21 @@ Rails.application.routes.draw do
         get '', to: "merchants#index"
         get '/find', to: "search#show"
         get '/find_all', to: "search#index"
+        get '/random' to: "random#show"
         get '/:id', to: "merchants#show"
       end
       namespace :customers do
         get '', to: "customers#index"
         get '/find', to: "search#show"
         get '/find_all', to: "search#index"
+        get '/random' to: "random#show"
         get '/:id', to: "customers#show"
       end
       namespace :transactions do
         get '', to: "transactions#index"
         get '/find', to: "search#show"
         get '/find_all', to: "search#index"
+        get '/random' to: "random#show"
         get '/:id', to: "transactions#show"
       end
     end
