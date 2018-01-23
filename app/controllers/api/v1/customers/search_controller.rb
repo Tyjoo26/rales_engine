90d2c71b9eb89513500f006d5eaef79c8261a550
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-23 13:48:27 -0700

    Modify for strong params

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -2,21 +2,16 @@ class Api::V1::Customers::SearchController < ApplicationController
 
 
   def show
-    if params["name"]
-      render json: Customer.find_by(first_name: params["name"])
-    else params["id"]
-      render json: Customer.find(params[:id])
-    # elsif params["created_at"]
-    #   render json: Customer.find_by(created_at: )
-    end
+    render json: Customer.where(customer_params).first
   end
 
   def index
-    if params["name"]
-      render json: Customer.where(first_name: params["name"])
-    else params["id"]
-      render json: Customer.where(id: params["id"])
-    # elsif params["created_at"]
-    end
+    render json: Customer.where(customer_params)
+  end
+
+  private
+
+  def customer_params
+    params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
   end
 end
