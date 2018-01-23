ec87323ccecf5c8a63395211d2c4e432fb29ea01
maxscores <maxstackhouse@gmail.com>
2018-01-23 12:51:09 -0700

    Fix rounding error

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::Items::SearchController < ApplicationController
-  def show
-    render json: Item.order("id ASC").where(search_params).first
-  end
-
   def index
     render json: Item.order("id ASC").where(search_params)
   end
 
+  def show
+    render json: Item.order("id ASC").where(search_params).first
+  end
+
   private
     def search_params
-      params[:unit_price] = price_to_integer(params[:unit_price])  if params[:unit_price]
+      params[:unit_price] = price_to_integer(params[:unit_price]) if params[:unit_price]
       params.permit(:id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at )
     end
 end
