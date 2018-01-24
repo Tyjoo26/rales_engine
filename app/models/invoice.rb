55bf4b83ceeac7cb4b7d06d6f538b676e41cd4b0
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-23 17:59:48 -0700

    Add more test for robust testing functionality

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,5 +2,6 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
 
+  has_many :transactions
   enum status: ["shipped"]
 end
---
+++ spec/requests/api/v1/merchants/revenue_request_spec.rb
@@ -0,0 +1,5 @@
+describe "Api/v1/merchants/revenue Query Controller " do
+  it "provides #revenue for merchant" do
+
+  end
+end
