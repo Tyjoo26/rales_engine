eacc4c461f88aa6018af8808dbb4bd9c69380ccb
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-24 14:00:28 -0700

    refactored methods logic for active record queries

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -14,4 +14,22 @@ class Invoice < ApplicationRecord
       .where("invoices.updated_at <= ?", end_of_day(date))
       .sum("invoice_items.unit_price * invoice_items.quantity")
   end
+
+  def self.merchant_total_revenue_by_date(date, id)
+    joins(:transactions)
+      .joins(:invoice_items)
+      .merge(Transaction.successful)
+      .where(merchant_id: id)
+      .where("invoices.updated_at >= ?", start_of_day(date))
+      .where("invoices.updated_at <= ?", end_of_day(date))
+      .sum("invoice_items.unit_price * invoice_items.quantity")
+  end
+
+  def self.merchant_total_revenue(id)
+    joins(:transactions)
+      .joins(:invoice_items)
+      .merge(Transaction.successful)
+      .where(merchant_id: id)
+      .sum("invoice_items.unit_price * invoice_items.quantity")
+  end
 end
