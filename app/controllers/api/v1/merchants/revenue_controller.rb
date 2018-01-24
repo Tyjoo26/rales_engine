e85c8431a56c92695b8ef8606512aac4d7b2d306
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-24 14:00:16 -0700

    Revenue controller methods are refactored with logic in invoice.rb, spec harnesses passes for merchant total revenue specific, and by date

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,15 +1,11 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
-    if params[:date] == nil
-      merchant = Invoice.joins(:transactions).joins(:invoice_items).merge(Transaction.successful).where(merchant_id: params[:id]).sum("invoice_items.unit_price * invoice_items.quantity")
+    if params[:date]
+      revenue = Invoice.merchant_total_revenue_by_date(params[:date], params[:id].to_i)
     else
-      merchant = Invoice.joins(:transactions).joins(:invoice_items).where(transactions: {result: 0}).where(merchant_id: params[:id]).where().sum("invoice_items.unit_price * invoice_items.quantity")
+      revenue = Invoice.merchant_total_revenue(params[:id].to_i)
     end
-    render json: {"revenue" => revenue_in_dollars(merchant)}
+    render json: {"revenue" => revenue_in_dollars(revenue)}
   end
-
 end
-
-
-Merchant.find(27).transactions.where(result: "0").map {|transaction| InvoiceItem.where(invoice_id: transaction.invoice_id)}.flatten.map {|invoiceitem| invoiceitem.unit_price * invoiceitem.quantity}.sum
