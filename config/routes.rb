ec77d7142636f92e58567bc90b2d1755aac7fc62
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-24 16:56:46 -0700

    Modify routes for most items controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
         get '/find_all', to: "search#index"
         get '/random', to: "random#show"
         get '/most_revenue', to: "most_revenue#index"
+        get '/most_items', to: "most_items#index"
         get '/revenue', to: "total_revenue#index"
         get '/:id', to: "merchants#show"
         get '/:id/favorite_customer', to: "favorite_customer#show"
