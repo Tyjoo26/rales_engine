6aa211cfbf4d32643b66da411c9075f3dc7941fd
maxscores <maxstackhouse@gmail.com>
2018-01-25 09:12:20 -0700

    Add jquery invocie items fo realz

--- public/js/invoice_items.js
+++ public/js/invoice_items.js
@@ -36,7 +36,38 @@ $("#one-invoice-item").click(function() {
 });
 
 var invoiceItemSearchParams = `
-
+<table>
+  <th>Search Field</th>
+  <th>Example Parameter</th>
+  <tr>
+    <td>invoice item id</td>
+    <td>id=27</td>
+  </tr>
+  <tr>
+    <td>item id</td>
+    <td>item_id=54</td>
+  </tr>
+  <tr>
+    <td>invoice id</td>
+    <td>invoice_id=43</td>
+  </tr>
+  <tr>
+    <td>quantity</td>
+    <td>quantity=5</td>
+  </tr>
+  <tr>
+    <td>unit_price</td>
+    <td>unit_price=19.99</td>
+  </tr>
+  <tr>
+    <td>created at</td>
+    <td>created_at="2012-03-27T14:56:04.000Z"</td>
+  </tr>
+  <tr>
+    <td>updated_at</td>
+    <td>updated_at="2012-03-27T14:56:04.000Z"</td>
+  </tr>
+</table>
 `
 
 var searchManyInvoiceItems = ["GET /api/v1/invoice_items/find_all?parameter", [
@@ -56,6 +87,12 @@ var searchManyInvoiceItems = ["GET /api/v1/invoice_items/find_all?parameter", [
   }
 ]];
 
+$("#search-many-invoice-items").click(function() {
+  $("#search-params").html("")
+  $("#example-request").html(searchManyInvoiceItems[0])
+  $("#example-output").html(JSON.stringify(searchManyInvoiceItems[1], undefined, 2))
+});
+
 var searchOneInvoiceItem = ["GET /api/v1/invoice_items/find?parameter", {
   "id": 135,
   "item_id": 1,
@@ -63,3 +100,9 @@ var searchOneInvoiceItem = ["GET /api/v1/invoice_items/find?parameter", {
   "quantity": 2,
   "unit_price": "751.07"
 }];
+
+$("#search-one-invoice-item").click(function() {
+  $("#search-params").html("")
+  $("#example-request").html(searchOneInvoiceItem[0])
+  $("#example-output").html(JSON.stringify(searchOneInvoiceItem[1], undefined, 2))
+});
