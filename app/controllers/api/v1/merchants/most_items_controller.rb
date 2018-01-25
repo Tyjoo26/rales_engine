d4fe519ec4b05df8091ca70dff2a9d865a71cf05
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-24 17:04:37 -0700

    Refactor index method by moving active record logic into invoice model

--- app/controllers/api/v1/merchants/most_items_controller.rb
+++ app/controllers/api/v1/merchants/most_items_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Merchants::MostItemsController < ApplicationController
 
   def index
-    merchants = Invoice.select("merchants.*, sum(invoice_items.quantity) AS total_items").joins(:invoice_items).joins(:merchant).joins(:transactions).merge(Transaction.successful).group("merchants.id").order("total_items DESC").limit(params[:quantity])
+    merchants = Invoice.most_items_sold(params[:quantity])
 
     render json: merchants, each_serializer: MerchantSerializer
   end
