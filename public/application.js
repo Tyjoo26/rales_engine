eee2a010a7e014210689fa32bae9b87c1f188f08
maxscores <maxstackhouse@gmail.com>
2018-02-04 12:24:31 -0700

    Build flexible JS method

--- public/application.js
+++ public/application.js
@@ -6,6 +6,9 @@ $.getScript("js/items.js", function() {});
 $.getScript("js/invoice_items.js", function() {});
 $.getScript("js/business_intelligence.js", function() {});
 
+function kebabToCamel(string){
+  return string.replace(/(\-\w)/g, function(m){return m[1].toUpperCase();})
+}
 
 $.fn.showResult = function(endpoint) {
   $("#search-params").html(endpoint[0]);
@@ -14,37 +17,5 @@ $.fn.showResult = function(endpoint) {
 };
 
 $(".endpoint").click(function() {
-  $.fn.showResult(name)
-});
-
-// $("#all-transactions").click(function() {
-//   $("#search-params").html(allTransactions[0])
-//   $("#example-request").html(allTransactions[1])
-//   $("#example-output").html(JSON.stringify(allTransactions[2], undefined, 2))
-// });
-
-// $("#one-transaction").click(function() {
-//   $("#search-params").html("")
-//   $("#example-request").html(oneTransaction[0])
-//   $("#example-output").html(JSON.stringify(oneTransaction[1], undefined, 2))
-// });
-
-
-$("#search-many-transactions").click(function() {
-  $("#search-params").html(transactionSearchParams)
-  $("#example-request").html(searchAllTransactions[0])
-  $("#example-output").html(JSON.stringify(searchAllTransactions[1], undefined, 2))
-});
-
-
-$("#search-one-transaction").click(function() {
-  $("#search-params").html(transactionSearchParams)
-  $("#example-request").html(searchOneTransaction[0])
-  $("#example-output").html(JSON.stringify(searchOneTransaction[1], undefined, 2))
-});
-
-$("#transaction-invoice").click(function() {
-  $("#search-params").html("")
-  $("#example-request").html(transactionInvoice[0])
-  $("#example-output").html(JSON.stringify(transactionInvoice[1], undefined, 2))
+  $.fn.showResult(eval(kebabToCamel($(this).attr('id'))))
 });
